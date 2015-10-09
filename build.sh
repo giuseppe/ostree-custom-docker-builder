c04a74942768e43c25a2d09bb8ec753024da9449
Giuseppe Scrivano <gscrivan@redhat.com>
2015-10-09 14:33:12 +0200

    build.sh: do not recreate the image if it already exist for a given OSTree commit
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- build.sh
+++ build.sh
@@ -5,26 +5,41 @@ IFS=$'\n\t'
 DOCKER_SOCKET=/var/run/docker.sock
 
 if [ ! -e "${DOCKER_SOCKET}" ]; then
-  echo "Docker socket missing at ${DOCKER_SOCKET}"
-  exit 1
+    echo "Docker socket missing at ${DOCKER_SOCKET}"
+    exit 1
 fi
 
 if [ -n "${OUTPUT_IMAGE}" ]; then
-  TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
+    TAG="${OUTPUT_REGISTRY}/${OUTPUT_IMAGE}"
 fi
 
 mkdir repo
 ostree --repo=repo init --mode=archive-z2
 ostree --repo=repo remote add origin --no-gpg-verify "${SOURCE_REPOSITORY}"
 ostree --repo=repo pull origin "${SOURCE_REF}"
-ostree --repo=repo checkout --fsync=no "${SOURCE_REF}" checkout
+COMMIT=$(ostree --repo=repo rev-parse "${SOURCE_REF}")
 
-tar -C checkout -c -f- . | docker import - "${TAG}"
+docker pull "${TAG}"
+
+if test -z "$(docker images --filter label=ostree.commit=${COMMIT} | tail -n+2)"; then
+    mkdir image
+    ostree --repo=repo checkout --fsync=no "${SOURCE_REF}" image/sysroot
+
+    cat > image/Dockerfile << EOF
+FROM scratch
+ADD sysroot /
+LABEL ostree.commit=${COMMIT}
+EOF
+    docker build -t "${TAG}" image
+else
+    IMAGE="$(docker images --filter label=ostree.commit=${COMMIT} | tail -n+2 | awk '{print $3}')"
+    docker tag "${IMAGE}" "${TAG}"
+fi
 
 if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
-  cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
+    cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
 fi
 
 if [ -n "${OUTPUT_IMAGE}" ] || [ -s "/root/.dockercfg" ]; then
-  docker push "${TAG}"
+    docker push "${TAG}"
 fi
